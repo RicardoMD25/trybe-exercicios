78c42928de6ae7a19fb99591150891db19f0a561
Ricardo <ricardo_md_25@hotmail.com>
2022-04-06 18:52:59 -0300

    parte 2 e 3

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,11 +1,42 @@
+const a = 5;
+const b = 2;
+const c = 12;
+
+
 //primeiro exercicio
 
-const a = 5
-const b = 2
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log(a % b)
+// segundo exercicio
+
+if (a > b) {
+    console.log("a é maior");
+} else {
+    console.log("b é maior");
+}
+
+// terceiro exercicio
+
+if (a > b && a > c) {
+    console.log("a é maior");
+} else if (b > a && b > a) {
+    console.log("b é maior");
+} else {
+    console.log("c é maior")
+}
+
+
+
+
+
+
+
+
+
+
+
 
