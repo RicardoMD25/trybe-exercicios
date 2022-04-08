f703eac628d703eedf6e22d16e472ffee912e845
Ricardo <ricardo_md_25@hotmail.com>
2022-04-08 00:24:57 -0300

    parte 9

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script8 e 9.js
@@ -0,0 +1,20 @@
+const n1 = 2;
+const n2 = 3;
+const n3 = 6;
+
+
+if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+// parte 9
+
+
+if (n1 % 2 !== 0 || n2 % 2 !== 0 || n3 % 2 !== 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
