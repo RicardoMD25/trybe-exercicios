22502678eebac91a7a3e9134a67d34d463c4d2d8
Ricardo <ricardo_md_25@hotmail.com>
2022-04-11 20:44:01 -0300

    exercicio 2 - parte 1 a 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -16,4 +16,44 @@ function Palindrome(string) {
 
   //exercicio 2
 
+  function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMaior] < numeros[indice]) {
+        indiceMaior = indice;
+      }
+    }
+    return indiceMaior;
+  }
   
+  console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
+
+
+// exercicios 3
+
+function indiceDoMenor(numeros) {
+    let indiceMenor = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMenor] > numeros[indice]) {
+        indiceMenor = indice;
+      }
+    }
+    return indiceMenor;
+  }
+  
+  console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3]));
+
+
+  //exercicio 4
+
+  function maiorNome(nomeDePessoas){
+    let maiorNome = nomeDePessoas[0];
+    for(let index in nomeDePessoas){
+        if (maiorNome.length < nomeDePessoas[index].length) {
+            maiorNome = nomeDePessoas[index];
+        }
+    }
+    return maiorNome;
+  }
+
+  console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
